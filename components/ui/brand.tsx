fc1763fba994711560acac78bd51e402c9bc2837
sathish <gs.techno.in@gmail.com>
2024-04-19 17:08:19 +0100

    v2

--- components/ui/brand.tsx
+++ components/ui/brand.tsx
@@ -3,6 +3,7 @@
 import Link from "next/link"
 import { FC } from "react"
 import { ChatbotUISVG } from "../icons/chatbotui-svg"
+import { MantisUISVG } from "../icons/mantis-svg"
 
 interface BrandProps {
   theme?: "dark" | "light"
@@ -12,15 +13,15 @@ export const Brand: FC<BrandProps> = ({ theme = "dark" }) => {
   return (
     <Link
       className="flex cursor-pointer flex-col items-center hover:opacity-50"
-      href="https://www.chatbotui.com"
+      href="https://www.datamantis.ai"
       target="_blank"
       rel="noopener noreferrer"
     >
       <div className="mb-2">
-        <ChatbotUISVG theme={theme === "dark" ? "dark" : "light"} scale={0.3} />
+        <MantisUISVG theme={theme === "dark" ? "dark" : "light"} scale={0.3} />
       </div>
 
-      <div className="text-4xl font-bold tracking-wide">Chatbot UI</div>
+      <div className="text-4xl font-bold tracking-wide">Data Mantis</div>
     </Link>
   )
 }
--- supabase/migrations/20240302004845_add_groq.sql
+++ supabase/migrations/20240302004845_add_groq.sql
@@ -1,2 +1,2 @@
-ALTER TABLE profiles ADD COLUMN ollama_api_key TEXT CHECK (char_length(groq_api_key) <= 1000);
+ALTER TABLE profiles ADD COLUMN ollama_api_key TEXT CHECK (char_length(ollama_api_key) <= 1000);
 ALTER TABLE profiles ADD COLUMN groq_api_key TEXT CHECK (char_length(groq_api_key) <= 1000);
