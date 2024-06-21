b01348992153361ccb352598dd8fda6a77d8118d
sathish <gs.techno.in@gmail.com>
2024-06-22 00:03:26 +0100

    test

--- supabase/migrations/20240302004845_add_groq.sql
+++ supabase/migrations/20240302004845_add_groq.sql
@@ -1,2 +1,3 @@
 ALTER TABLE profiles ADD COLUMN ollama_api_key TEXT CHECK (char_length(ollama_api_key) <= 1000);
 ALTER TABLE profiles ADD COLUMN groq_api_key TEXT CHECK (char_length(groq_api_key) <= 1000);
+-- test
